3aad71c8a9a5758e8a8cee6e35dd81d1b288d4f6
Fernand DelaGuardia <f.delaguardia@yahoo.com>
2023-02-02 14:17:11 -0500

    done

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+const hbs = require("hbs")
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+const PunkAPIWrapper = require("punkapi-javascript-wrapper")
+const punkAPI = new PunkAPIWrapper()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IronBeers' });
 });
+
+
+router.get("/beers", ((req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromApi) => {
+      console.log(beersFromApi)
+      res.render("beers", {beersFromApi})
+    })
+    .catch(error => console.log(error)); 
+}))
+
+
+router.get("/random-beer", ((req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then((responseFromAPI) => {
+      res.render("random-beer", {responseFromAPI})
+    })
+    .catch(error => console.log(error));
+}))
+
+
+router.get("/beer/:id", ((req, res, next) => {
+  punkAPI
+    .getBeer(req.params.id)
+    .then((result) => {
+      res.render("beerdetail", {result})
+    })
+    .catch(error => console.log(error));
+}))
+
 
 module.exports = router;
